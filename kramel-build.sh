0cc783a3ec8b78a0f5bce16432f715b73e9ebf61
farizmaul <nickshade88@gmail.com>
2022-03-17 18:46:32 +0700

    CI: Update to panchajanya19999 archlinux docker
    Update script and sync
    
    Signed-off-by: farizmaul <nickshade88@gmail.com>

--- kramel-build.sh
+++ kramel-build.sh
@@ -4,14 +4,13 @@ git clone --depth=1 https://github.com/farizmaul/AnyKernel3.git
 
 IMAGE=$(pwd)/out/arch/arm64/boot/Image
 DTBO=$(pwd)/out/arch/arm64/boot/dtbo.img
-DTB=$(pwd)/out/arch/arm64/boot/dtb
 START=$(date +"%s")
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-VERSION=ZeeMoy
+VERSION=Jasmine
 TANGGAL=${VERSION}-$(TZ=Asia/Jakarta date "+%Y%m%d-%H%M")
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_BUILD_HOST="cyber"
+export KBUILD_BUILD_HOST="android"
 export KBUILD_BUILD_USER="ricoayuba"
 #main group
 export chat_id="-1001726996867"
@@ -42,9 +41,12 @@ cd ../
     make -j$(nproc --all) O=out \
 				ARCH=arm64 \
 				LOCALVERSION=-${TANGGAL} \
+				AR=llvm-ar \
+				OBJDUMP=llvm-objdump \
+				STRIP=llvm-strip \
 				CROSS_COMPILE_ARM32=arm-eabi- \
 				CROSS_COMPILE=aarch64-elf- \
-				LD=aarch64-elf-ld.lld 2>&1 | tee build.log
+				LD=ld.lld 2>&1 | tee build.log
 
 END=$(date +"%s")
 DIFF=$((END - START))
@@ -59,7 +61,6 @@ Latest Commit: <code>$(git log --pretty=format:'%h : %s' -1)</code>
 
 cp ${IMAGE} $(pwd)/AnyKernel3
 cp ${DTBO} $(pwd)/AnyKernel3
-cp ${DTB} $(pwd)/AnyKernel3
 
         cd AnyKernel3
         zip -r9 HyperX-${TANGGAL}.zip * --exclude *.jar
@@ -72,7 +73,7 @@ cp ${DTB} $(pwd)/AnyKernel3
         curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendSticker" \
         -d sticker="CAACAgUAAxkBAAJi017AAw5j25_B3m8IP-iy98ffcGHZAAJAAgACeV4XIusNfRHZD3hnGQQ" \
         -d chat_id="$chat_id"
-	
+
 	curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage -d text="hi guys, the latest update is available on @HyperX_Archive !" -d chat_id=${chat_id2} -d parse_mode=HTML
 
 cd ..
